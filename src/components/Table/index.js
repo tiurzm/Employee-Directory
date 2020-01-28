b33326aeb930d9e5568bd00c78105a02842e1cb6
Tiur Mawaddah <tiurzm@gmail.com>
2020-01-28 08:18:51 -0600

    working on the employee's data

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -12,7 +12,153 @@ function Table(props) {
                     <th scope="col">DOB</th>
                 </tr>
             </thead>
-            {props.children}
+            <tbody>
+                <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                    <td>@jkk</td>
+                </tr>
+                <tr>
+                    <th scope="row">2</th>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                    <td>@bbb</td>
+                </tr>
+                <tr>
+                    <th scope="row">3</th>
+                    <td>Larry</td>
+                    <td>the Bird</td>
+                    <td>@twitter</td>
+                    <td>@tnt</td>
+
+                </tr>
+                <tr>
+                    <th scope="row">4</th>
+                    <td>Larry</td>
+                    <td>the Bird</td>
+                    <td>@lol</td>
+                    <td>@dsa</td>
+                </tr>
+                <tr>
+                    <th scope="row">5</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                    <td>@jkk</td>
+                </tr>
+                <tr>
+                    <th scope="row">6</th>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                    <td>@bbb</td>
+                </tr>
+                <tr>
+                    <th scope="row">7</th>
+                    <td>Larry</td>
+                    <td>the Bird</td>
+                    <td>@twitter</td>
+                    <td>@tnt</td>
+
+                </tr>
+                <tr>
+                    <th scope="row">8</th>
+                    <td>Larry</td>
+                    <td>the Bird</td>
+                    <td>@lol</td>
+                    <td>@dsa</td>
+                </tr>
+                <tr>
+                    <th scope="row">9</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                    <td>@jkk</td>
+                </tr>
+                <tr>
+                    <th scope="row">10</th>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                    <td>@bbb</td>
+                </tr>
+                <tr>
+                    <th scope="row">11</th>
+                    <td>Larry</td>
+                    <td>the Bird</td>
+                    <td>@twitter</td>
+                    <td>@tnt</td>
+
+                </tr>
+                <tr>
+                    <th scope="row">12</th>
+                    <td>Larry</td>
+                    <td>the Bird</td>
+                    <td>@lol</td>
+                    <td>@dsa</td>
+                </tr>
+                <tr>
+                    <th scope="row">13</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                    <td>@jkk</td>
+                </tr>
+                <tr>
+                    <th scope="row">14</th>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                    <td>@bbb</td>
+                </tr>
+                <tr>
+                    <th scope="row">15</th>
+                    <td>Larry</td>
+                    <td>the Bird</td>
+                    <td>@twitter</td>
+                    <td>@tnt</td>
+
+                </tr>
+                <tr>
+                    <th scope="row">16</th>
+                    <td>Larry</td>
+                    <td>the Bird</td>
+                    <td>@lol</td>
+                    <td>@dsa</td>
+                </tr>
+                <tr>
+                    <th scope="row">17</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                    <td>@jkk</td>
+                </tr>
+                <tr>
+                    <th scope="row">18</th>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                    <td>@bbb</td>
+                </tr>
+                <tr>
+                    <th scope="row">19</th>
+                    <td>Larry</td>
+                    <td>the Bird</td>
+                    <td>@twitter</td>
+                    <td>@tnt</td>
+
+                </tr>
+                <tr>
+                    <th scope="row">20</th>
+                    <td>Larry</td>
+                    <td>the Bird</td>
+                    <td>@lol</td>
+                    <td>@dsa</td>
+                </tr>
+            </tbody>
         </table>
     );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,37 +4,6 @@ import Table from "../components/Table";
 function Home() {
     return (
         <Table>
-            <tbody>
-                <tr>
-                    <th scope="row">1</th>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                    <td>@jkk</td>
-                </tr>
-                <tr>
-                    <th scope="row">2</th>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                    <td>@bbb</td>
-                </tr>
-                <tr>
-                    <th scope="row">3</th>
-                    <td>Larry</td>
-                    <td>the Bird</td>
-                    <td>@twitter</td>
-                    <td>@tnt</td>
-
-                </tr>
-                <tr>
-                    <th scope="row">4</th>
-                    <td>Larry</td>
-                    <td>the Bird</td>
-                    <td>@lol</td>
-                    <td>@dsa</td>
-                </tr>
-            </tbody>
         </Table>
     );
 };
